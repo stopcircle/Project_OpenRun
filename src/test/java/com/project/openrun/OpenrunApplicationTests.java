0854841ad3e2121e88ef5efea16a398eacffb934
stopcircle <jjy011007@gmail.com>
2026-02-02 22:41:57 +0900

    Initial commit

---
+++ src/test/java/com/project/openrun/OpenrunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.openrun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenrunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
